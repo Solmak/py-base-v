32483f843e18cedb54dc6b228e367f2542664e79
Solmak <solvy@internet.ru>
2021-12-22 15:00:49 +0300

    lesson6_pt +fraud protection

--- lesson6_pt.py
+++ lesson6_pt.py
@@ -18,11 +18,12 @@ phrases = (
     'Возможно {}?\n',
     'Я уверен, что это {}!\n'
 )
-print('Загадайте число от 1 до 100\nЯ буду называть вам числа\nЕсли я угадаю - введите =\nЕсли ваше число больше - >\nЕсли ваше число меньше - <\nА кто будет жульничать, того будем бить по морде!\nПо наглой рыжей морде!')
+print('Загадайте число от 1 до 100\nЯ буду называть вам числа\nЕсли я угадаю - введите =\nЕсли моё число больше - >\nЕсли моё число меньше - <\nА кто будет жульничать, того будем бить по морде!\nПо наглой рыжей морде!')
 # Для проверки ввода пользователя
-range_begin = 0
-range_end = 100
 user_answers = ('=','<','>')
+# Начальный диапазон
+range_begin = 1
+range_end = 100
 
 # Основной цикл
 user_input = None
@@ -30,16 +31,22 @@ count = 0
 while user_input != '=' :
     count += 1
     user_input = None
-    #начало конец
+    #начало конец Не стал делать метод половин - очень однообразно выходит
     number = random.randint(range_begin, range_end)  
     while not user_input in user_answers:
         user_input = input(phrases[random.randint(0, len(phrases) - 1)].format(number)) # учим разговаривать
         if not user_input in user_answers: # исключительно для пояснения неверных действий
             print('Может тебе непонятьно? Можно только "=", ">" или "<"')
-    # сужаем диапазон
+    # сужаем диапазон 
     if user_input == '>':
+        if number <= range_begin:
+            print('Рыжая морда! Вы шулер! Я с вами не играю!')
+            break
         range_end = number - 1
     elif user_input == '<':
+        if number >= range_end:
+            print('Подлый лис! Опять шельмуешь?! Я с тобой больше не играю!')
+            break
         range_begin = number + 1
 
 else:
