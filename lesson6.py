19b2b9c9b1f698eab783e542a2ab10c75aaedd75
Solmak <solvy@internet.ru>
2021-12-22 11:19:53 +0300

    lesson6 while update

--- lesson6.py
+++ lesson6.py
@@ -4,17 +4,16 @@ import random
 number = random.randint(1, 100)
 # ! print (number)
 
-# First version
-while True:
+#  Version 2
+while user_number != number :
     # Шаг 2. Предложить пользователю угадать число
     # Suggest the user to guess the number
     user_number = int(input('Enter a number from 1 to 100: '))
     # ! print (user_number)
     # Step 3. Comparing numbers, output of results
-    if user_number == number :
-        print('You win!')
-        break
-    elif user_number >  number :
+    if user_number >  number :
         print('Your number is bigger then the one you made')
-    else :
+    elif user_number <  number :
         print('Your number is less than the desired one')
+
+print('You win!')
