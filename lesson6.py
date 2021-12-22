1b55512be515dfef6040008558b82aba97ccf5da
Solmak <solvy@internet.ru>
2021-12-22 11:48:55 +0300

    lesson6 +limit the number of attempts

--- lesson6.py
+++ lesson6.py
@@ -2,20 +2,25 @@
 import random
 number = random.randint(1, 100)
 # ! print (number)
-#  Version 3 +количество попыток
+#  Version 4 +limit the number of attempts
 user_number = None
-tryCount = 0
+count = 0
+max_count = 3
 while user_number != number :
-    tryCount += 1
-    print(f'Attempt № {tryCount}')
+    count += 1
+    if count > max_count:
+        print('You`ve lost!')
+        break
+    print(f'Attempt № {count}')
     # Шаг 2. Предложить пользователю угадать число
     # Suggest the user to guess the number
     user_number = int(input('Enter a number from 1 to 100: '))
     # ! print (user_number)
     # Step 3. Comparing numbers, output of results
     if user_number >  number :
-        print('Your number is bigger then the one you made')
+        print('Your number is BIGGER then the one you made')
     elif user_number <  number :
-        print('Your number is less than the desired one')
+        print('Your number is LESS than the desired one')
+else :
+    print('You win!')
 
-print('You win!')
