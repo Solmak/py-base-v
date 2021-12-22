d66bdbb2dc58e79ad34bcead26f29e2cea275c74
Solmak <solvy@internet.ru>
2021-12-22 12:48:14 +0300

    lesson6 +add player`s names and number of players

--- lesson6.py
+++ lesson6.py
@@ -2,12 +2,21 @@
 import random
 number = random.randint(1, 100)
 # ! print (number)
-#  Version 5 +difficulty level
+# TODO: Version 6 +multiplayer
 user_number = None
 count = 0
 levels = {1: 10, 2: 5, 3: 3} # difficulty level: number of attempts
 level = int(input('Сhoose the difficulty level (1-3): '))
 max_count = levels[level]
+
+user_count = int(input('Enter the number of players: '))
+users = []
+for i in range(user_count) :
+    user_name = input(f'Enter the player`s name {i+1}: ')
+    users.append(user_name)
+# ! 
+print(users)
+
 while user_number != number :
     count += 1
     if count > max_count:
