e8dea6fdb1d5d09bda7c265065f8e2978505e542
Solmak <solvy@internet.ru>
2021-12-22 14:42:47 +0300

    lesson6_pt narrowing range

--- lesson6_pt.py
+++ lesson6_pt.py
@@ -22,7 +22,7 @@ print('Загадайте число от 1 до 100\nЯ буду называт
 # Для проверки ввода пользователя
 range_begin = 0
 range_end = 100
-user_answer = ('=','<','>')
+user_answers = ('=','<','>')
 
 # Основной цикл
 user_input = None
@@ -30,12 +30,18 @@ count = 0
 while user_input != '=' :
     count += 1
     user_input = None
-#начало конец
+    #начало конец
     number = random.randint(range_begin, range_end)  
-    while not user_input in user_answer:
+    while not user_input in user_answers:
         user_input = input(phrases[random.randint(0, len(phrases) - 1)].format(number)) # учим разговаривать
-        if not user_input in user_answer: # исключительно для пояснения неверных действий
+        if not user_input in user_answers: # исключительно для пояснения неверных действий
             print('Может тебе непонятьно? Можно только "=", ">" или "<"')
+    # сужаем диапазон
+    if user_input == '>':
+        range_end = number - 1
+    elif user_input == '<':
+        range_begin = number + 1
+
 else:
     print(f'Ха! Я тебя сделал всего за {count} попыток!')
 
