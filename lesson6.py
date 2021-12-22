7381360bb82278a05f7058011773c6523f0315f3
Solmak <solvy@internet.ru>
2021-12-22 13:15:36 +0300

    lesson6 +multiplayer version

--- lesson6.py
+++ lesson6.py
@@ -1,10 +1,10 @@
 # Шаг 1. Загадать число +
 import random
 number = random.randint(1, 100)
-# ! print (number)
 # TODO: Version 6 +multiplayer
 user_number = None
 count = 0
+
 levels = {1: 10, 2: 5, 3: 3} # difficulty level: number of attempts
 level = int(input('Сhoose the difficulty level (1-3): '))
 max_count = levels[level]
@@ -14,24 +14,30 @@ users = []
 for i in range(user_count) :
     user_name = input(f'Enter the player`s name {i+1}: ')
     users.append(user_name)
-# ! 
-print(users)
 
-while user_number != number :
+is_winner = False
+winner_name = None
+
+while not is_winner :
     count += 1
     if count > max_count:
-        print('You`ve lost!')
+        print('All the players lost!')
         break
     print(f'Attempt № {count}')
     # Шаг 2. Предложить пользователю угадать число
     # Suggest the user to guess the number
-    user_number = int(input('Enter a number from 1 to 100: '))
-    # ! print (user_number)
-    # Step 3. Comparing numbers, output of results
-    if user_number >  number :
-        print('Your number is BIGGER then the one you made')
-    elif user_number <  number :
-        print('Your number is LESS than the desired one')
+    for user in users:
+        print (f'{user}`s move')
+        user_number = int(input('Enter a number from 1 to 100: '))
+        # Step 3. Comparing numbers, output of results
+        if user_number == number :
+            is_winner = True
+            winner_name = user
+            break
+        elif user_number >  number :
+            print('Your number is BIGGER then the one you made')
+        else : #user_number <  number 
+            print('Your number is LESS than the desired one')
 else :
-    print('You win!')
+    print(f'You win, {winner_name}')
 
